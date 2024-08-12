1a2f7527cfe3b4856337bff6527950fdbb177b0d
Stas <adm.job@yandex.ru>
2024-08-12 15:26:26 +0500

    It works , but not correctly

--- ListTraining.cs
+++ ListTraining.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Training
@@ -23,18 +24,24 @@ namespace Training
         {
             string stringAll = "";
             List <string> words = new List<string>();
-
-            words.Add(lines.Split(" "));
-
-            //foreach (string e in lines)
-            //if (char.IsUpper(e[0]))
+            //words.Add(lines.Split(" "));
+            foreach (string e in lines)
             {
-                Console.WriteLine(e);
-                stringAll += e;
+                foreach (string str in e.Split(' '))
+                {
+                    if (char.IsUpper(str[0]))
+                    { 
+                        words.Add(str);
+                    }
+                }
             }
+            words.Reverse();
+            foreach (string str in words) 
+            {
+                stringAll += str + " ";
+            }
+ 
             return stringAll;
-
-
             /* 
             List<int> list = new List<int>();
 
@@ -46,6 +53,18 @@ namespace Training
 
 	        foreach (var e in list)
 		        Console.WriteLine(e);
+
+            Для разбиения строки на слова можно использовать функцию text.Split(' ')
+
+            Вы можете проверить, является ли символ заглавным с помощью Char.IsUpper
+
+            Используйте List<string>, чтобы сохранить все найденные в тексте слова с большой буквы
+
+            У списка есть метод Reverse(), переворачивающий список
+
+            Склеить отдельные слова в текст можно ещё одним циклом, а можно специальным методом string.Join(" ", words.ToArray())
+
+            Имейте в виду, "".Split(' ') возвращает пустую строку! Обращение к нулевому символу пустой строки приведёт к ошибке ArgumentOutOfRangeException.
             */
         }
     }
